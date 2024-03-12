8502d5020ca74a24c722d23f3463829ab4120bf1
umarkhanwah <umarkhanwah044@gmail.com>
2024-03-12 10:16:03 +0500

    Product Crud Done ,

--- ShradhaBookStore/Controllers/AdminController.cs
+++ ShradhaBookStore/Controllers/AdminController.cs
@@ -74,11 +74,12 @@ namespace ShradhaBookStore.Controllers
                 return RedirectToAction("Login", "User");
             }
             var categories = bookStoreContext.Categories.ToList();
+            ViewData["msg"] = TempData["msg"];
             return View(categories);
         }
         public IActionResult Update_Category(int id)
         {
-            var category = bookStoreContext.Categories.Find(id);
+            var category = bookStoreContext.Categories.First(x=>x.Id == id);
             var categoriesWithoutParent = bookStoreContext.Categories
                                                .Where(x => x.ParentCategoryId == null)
                                                 .Select(c => new SelectListItem
@@ -131,13 +132,14 @@ namespace ShradhaBookStore.Controllers
             category.Status = "Disabled"; // Assuming there's a Status property in your Category model
             bookStoreContext.Categories.Update(category);
             bookStoreContext.SaveChanges();
+            TempData["msg"] = "Category Disabled Successfully";
 
             return RedirectToAction("Show_Categories"); // Redirect to the categories list or any other suitable action
         }
         [HttpPost]
         public IActionResult ConfirmDisable(int CategoryId)
         {
-            var category = bookStoreContext.Categories.Find(CategoryId);
+            var category = bookStoreContext.Categories.First(x => x.Id == CategoryId);
             var subs = bookStoreContext.Categories.Where(x => x.ParentCategoryId == CategoryId).ToList();
             var products = bookStoreContext.Products.Where(x => x.CategoryId == CategoryId).ToList();
 
@@ -160,7 +162,7 @@ namespace ShradhaBookStore.Controllers
             bookStoreContext.SaveChanges();
 
             // Redirect to a suitable action
-            return RedirectToAction("Index", "Categories"); // Redirect to the categories list or any other suitable action
+            return RedirectToAction("Show_Categories"); // Redirect to the categories list or any other suitable action
         }
 
 
@@ -194,8 +196,23 @@ namespace ShradhaBookStore.Controllers
                                                 })
                                                 .ToList();
 
+            var manufacturers = bookStoreContext.Manufacturers
+                                              
+                                               .Select(c => new SelectListItem
+                                               {
+                                                   Value = c.Id.ToString(),
+                                                   Text = c.Name // Assuming there's a property called Name for category
+                                               })
+                                               .ToList();
             ViewBag.CategoryList = categories;
+            ViewBag.ManufacturerList = manufacturers;
+            var last_id = bookStoreContext.Products.OrderByDescending(x => x.Id).FirstOrDefault();
+            @ViewBag.AcronymsId = "0001";
+            if (last_id != null )
+            {
+                @ViewBag.AcronymsId = last_id.Id +1;
 
+            }
             return View();
         }
         [HttpPost]
@@ -205,11 +222,14 @@ namespace ShradhaBookStore.Controllers
             {
                 string uniquefilename = Product_Image(product);
                 product.Image = uniquefilename;
+                //Get Manufacturers Abbreviation and Concat With its ID
+                var manufacturer = bookStoreContext.Manufacturers.Find(product.ManufacturerId);
+                product.Acronym = manufacturer.Acronyms + product.Acronym;
                 bookStoreContext.Products.Add(product);
                 bookStoreContext.SaveChanges();
-                return RedirectToAction("Show_Categories");
+                return RedirectToAction("Show_Products");
             }
-            return RedirectToAction("Add_Category");
+            return RedirectToAction("Add_Product");
         }
         private string Product_Image(Product category)
         {
@@ -225,6 +245,82 @@ namespace ShradhaBookStore.Controllers
                 }
             }
             return uniquefilename;
+        }
+        public IActionResult Show_Products()
+        {
+            if (HttpContext.Session.GetString("adminsession") == null)
+            {
+                TempData["Error"] = "Please Login first";
+                return RedirectToAction("Login", "User");
+            }
+            ViewData["msg"] = TempData["msg"];
+            Allproduct allproduct = new Allproduct();   
+            var products = bookStoreContext.Products.ToList();
+            var categories = bookStoreContext.Categories.ToList();
+            var manufacturers = bookStoreContext.Manufacturers.ToList();
+            allproduct.Products = products;
+            allproduct.Categories = categories;
+            allproduct.Manufacturers = manufacturers;
+            return View(allproduct);
+        }
+        public IActionResult Update_Product(int id)
+        {
+            if (HttpContext.Session.GetString("adminsession") == null)
+            {
+                TempData["Error"] = "Please Login first";
+                return RedirectToAction("Login", "User");
+            }
+            var product = bookStoreContext.Products.First(x => x.Id == id);
+
+            var categories = bookStoreContext.Categories
+                                               .Where(x => x.ParentCategoryId != null)
+                                                .Select(c => new SelectListItem
+                                                {
+                                                    Value = c.Id.ToString(),
+                                                    Text = c.Name // Assuming there's a property called Name for category
+                                                })
+                                                .ToList();
+
+            var manufacturers = bookStoreContext.Manufacturers
+
+                                               .Select(c => new SelectListItem
+                                               {
+                                                   Value = c.Id.ToString(),
+                                                   Text = c.Name // Assuming there's a property called Name for category
+                                               })
+                                               .ToList();
+            ViewBag.CategoryList = categories;
+            ViewBag.ManufacturerList = manufacturers;
+
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Update_Product(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniquefilename = Product_Image(product);
+                product.Image = uniquefilename;
+                bookStoreContext.Products.Update(product);
+                bookStoreContext.SaveChanges();
+                TempData["msg"] = "Product Updated Successfully";
+                return RedirectToAction("Show_Products");
+            }
+            return RedirectToAction("Update_Product");
+        }
+        public IActionResult Deactivate_Product(int id)
+        {
+            var products = bookStoreContext.Products.First(x=>x.Id == id);
+            if (products == null)
+            {
+                TempData["msg"] = "Product Not Found";
+                return RedirectToAction("Show_Products");
+            }
+            products.Status = "Disabled";
+            bookStoreContext.Products.Update(products);
+            bookStoreContext.SaveChanges();
+            TempData["msg"] = "Product Deactivated";
+            return RedirectToAction("Show_Products");
         }
 
     }
